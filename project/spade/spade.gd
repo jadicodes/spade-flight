a435f89ee018c4e3b4ede7d5684b77ddd9ad4875
Jadi Miller <jadilmiller@gmail.com>
2024-04-12 12:46:10 -0400

    Add space bar tutorial

--- project/spade/spade.gd
+++ project/spade/spade.gd
@@ -1,26 +1,38 @@
 extends CharacterBody2D
 
+signal game_started
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -185.0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _game_started = false
 var _has_collided = false
 
 
-func _physics_process(delta) -> void:
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
-		Jukebox.play_flap()
-
-	_has_collided = move_and_collide(velocity * delta)
+func _ready():
+	$SpaceLabel.show()
 	
-	if _has_collided:
-		_kill()
+
+func _physics_process(delta) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		$SpaceLabel.hide()
+		_game_started = true
+		game_started.emit()
+
+	if _game_started:
+		var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+		if not is_on_floor():
+			velocity.y += gravity * delta
+
+
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			Jukebox.play_flap()
+
+		_has_collided = move_and_collide(velocity * delta)
+		
+		if _has_collided:
+			_kill()
 
 
 func _kill() -> void:
--- project/world/world.gd
+++ project/world/world.gd
@@ -1,17 +1,23 @@
 extends Node2D
 
+var _game_started = false
 
 @export var pipes_scene: PackedScene
 @onready var hud_scene = get_node("HUD")
 
 
 func _on_spawn_timer_timeout() -> void:
-	var pipe = pipes_scene.instantiate()
-	add_child(pipe)
-	pipe.add_to_score.connect(hud_scene._update_score)
-	pipe.position.y += _generate_pipe_y()
+	if _game_started:
+		var pipe = pipes_scene.instantiate()
+		add_child(pipe)
+		pipe.add_to_score.connect(hud_scene._update_score)
+		pipe.position.y += _generate_pipe_y()
 
 
 func _generate_pipe_y() -> int:
 	var _pipe_y: int = randi_range(-40, 40)
 	return _pipe_y
+
+
+func _on_spade_game_started():
+	_game_started = true
